53452f71dfb5e4a657d7f67f868bbab52c12c393
Sreya S Raj <sreyasraj06@gmail.com>
2020-11-22 15:01:55 +0530

    Update Games.js

--- src/components/pages/Games.js
+++ src/components/pages/Games.js
@@ -5,11 +5,12 @@ export default function Games()
       return <p className="gameo">
                 <h1>Game Library</h1><br/>
                 <h3>We got a huge library : )</h3><br /><br />
-      
-                <a href="#" classname="Games"><img src="images/img11.jpg"></img></a><br></br><h1>ATTATIC</h1><br></br><h3>COMING SOON..</h3><br /><br />
-                <a href="#" classname="Games"><img src="images/img10.jpg"></img></a><br></br><h1>DRAGON LAIR</h1><br></br><h3>COMING SOON..</h3><br /><br />
-                <a href="#" classname="Games"><img src="images/img9.jpg"></img></a><br></br><h1>THE UNKNOWN</h1><br></br><h3>COMING SOON..</h3><br /><br />
-                <a href="#" classname="Games"><img src="images/img13.jpeg"></img></a><br></br><h1>MARTIAN QUEST</h1><br></br><h3>COMING SOON..</h3><br /><br />
+                
+                <a href="#" classname="Games"><img src="https://images.unsplash.com/photo-1545548226-5fce973bef90?ixlib=rb-1.2.1&ixid=MXwxMjA3fDB8MHxzZWFyY2h8Nnx8dHJhbnNmb3JtZXJ8ZW58MHx8MHw%3D&auto=format&fit=crop&w=500&q=60"></img></a><br></br><h1>ATTATIC</h1><br></br><h3>COMING SOON..</h3><br /><br />
+                <a href="#" classname="Games"><img src="https://images.unsplash.com/photo-1568602562958-75fa1a44ab44?ixlib=rb-1.2.1&ixid=MXwxMjA3fDB8MHxzZWFyY2h8NXx8ZHJhZ29ufGVufDB8fDB8&auto=format&fit=crop&w=500&q=60"></img></a><br></br><h1>DRAGON LAIR</h1><br></br><h3>COMING SOON..</h3><br /><br />
+                <a href="#" classname="Games"><img src="https://images.unsplash.com/photo-1520915257752-86c8c5ff9a01?ixlib=rb-1.2.1&ixid=MXwxMjA3fDB8MHxzZWFyY2h8NDV8fHVua25vd258ZW58MHx8MHw%3D&auto=format&fit=crop&w=500&q=60"></img></a><br></br><h1>THE UNKNOWN</h1><br></br><h3>COMING SOON..</h3><br /><br />
+                <a href="#" classname="Games"><img src="https://images.unsplash.com/photo-1600297485496-4d5f07d8adb9?ixlib=rb-1.2.1&ixid=MXwxMjA3fDB8MHxzZWFyY2h8NHx8bWFydGlhbHxlbnwwfHwwfA%3D%3D&auto=format&fit=crop&w=500&q=60"></img></a><br></br><h1>MARTIAN QUEST</h1><br></br><h3>COMING SOON..</h3><br /><br />
+                
                 <br /><h1>Subscribe OUR NEWS LETTER</h1> <br />
                 <h3>Be the first to know about new games, exclusive previews & special promotions</h3><br />
                 <input id="email" type="email"  required placeholder="Enter your email"></input>
